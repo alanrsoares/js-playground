765931c4960384c495d3a8d00948e1856a8df82d
Alan R. Soares <alanrsoars@gmail.com>
2024-12-06 13:07:42 +0300

    chore: add alias support

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,9 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
+  resolve: {
+    alias: {
+      "~": new URL("./src", import.meta.url).pathname,
+    },
+  },
 });
