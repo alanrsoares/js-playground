8ffc6734fa4b39b266986f21fc362d1391488e4f
Alan R. Soares <alanrsoars@gmail.com>
2025-01-23 21:57:59 +1300

    fix: console proxy methods

--- src/utils/console.ts
+++ src/utils/console.ts
@@ -1,26 +1,21 @@
+const prettify = (arg: unknown) =>
+  typeof arg === "object" ? JSON.stringify(arg, null, 2) : String(arg);
+
 export function createConsoleProxy() {
   let output: string[] = [];
 
   const handler = {
     log: (...args: unknown[]) => {
-      output.push(
-        args
-          .map((arg) =>
-            typeof arg === "object"
-              ? JSON.stringify(arg, null, 2)
-              : String(arg),
-          )
-          .join(" "),
-      );
+      output.push(args.map(prettify).join(" "));
     },
     error: (...args: unknown[]) => {
-      output.push(`Error: ${args.map((arg) => String(arg)).join(" ")}`);
+      output.push(`Error: ${args.map(prettify).join(" ")}`);
     },
     warn: (...args: unknown[]) => {
-      output.push(`Warning: ${args.map((arg) => String(arg)).join(" ")}`);
+      output.push(`Warning: ${args.map(prettify).join(" ")}`);
     },
     info: (...args: unknown[]) => {
-      output.push(args.map((arg) => String(arg)).join(" "));
+      output.push(`Info: ${args.map(prettify).join(" ")}`);
     },
     clear: () => {
       output = [];
